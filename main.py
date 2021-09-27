8e97bc299e5b6713d9053287ed7b9cc3cdc46d4b
Lidiia-Afanasieva <67696613+Lidiia-Afanasieva@users.noreply.github.com>
2021-09-27 15:28:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-# 1942
 # создать два списка пробить их по циклу к каждому прибавить значение и глянуть по счётчику максимальное
 # нужно учитывать перемещение по обеим осям
 
